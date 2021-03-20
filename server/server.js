4f4dd5a875f0408ce77a1806ebec8d98f73a9ca7
phucciTr <miabam26@gmail.com>
2021-03-19 17:38:52 -0700

    Removed commented code

--- server/server.js
+++ server/server.js
@@ -3,19 +3,10 @@ const path = require('path');
 const cors = require('cors');
 const request = require('request');
 
-
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use(cors());
-
-
-
-// app.get('/images/org/:id', (req, res) => {
-//   // res.render('http://localhost:3004/ap p.js')
-//   res.redirect('http://localhost:3004/app.js');
-// });
-
 
 const server = app.listen(port, function () {
   console.log(`listenting on port:${port}`);
